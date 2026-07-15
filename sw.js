07ec84f9059571c7a1815962d575df8cb5f00550
sanajung <139295444+sanajung@users.noreply.github.com>
2026-07-15 14:56:32 +0900

    Add files via upload
    드래그앤드롭 단계 이동
    Ui 개선
    단계별 색 지정

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "checklist-app-v2";
+const CACHE_NAME = "checklist-app-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
